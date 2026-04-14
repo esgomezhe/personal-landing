aef8ad4908d1b4db9e4704075bfba16ecc35fd6c
Esteban Gomez <esgomezhe@unal.edu.co>
2026-04-14 17:19:34 -0500

    feat: add Material Symbols font and update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,15 +44,7 @@ export default function RootLayout({
       lang="es"
       className={`${spaceGrotesk.variable} ${inter.variable} ${robotoMono.variable} dark`}
     >
-      <body className="antialiased">
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
-        />
-        {children}
-      </body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
